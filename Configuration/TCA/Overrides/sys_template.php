9a4ff5b5e8af3988cd5c51a1b6e6206db44efa45
Michael Lang <115140776+hda-typo3@users.noreply.github.com>
2024-11-19 07:39:44 +0100

    Delete Configuration directory

--- Configuration/TCA/Overrides/sys_template.php
+++
@@ -1,4 +0,0 @@
-<?php
-defined('TYPO3_MODE') || die();
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('hda_personen', 'Configuration/TypoScript/Setup', 'h_da - Personen Plugin');
